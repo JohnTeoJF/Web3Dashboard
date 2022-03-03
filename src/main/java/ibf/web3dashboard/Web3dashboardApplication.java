b2604d67884950c0b422132414d3c8e02a1a5dc9
John <JohnTeoJF+git@gmail.com>
2022-03-04 06:56:49 +0800

    Initial

---
+++ src/main/java/ibf/web3dashboard/Web3dashboardApplication.java
@@ -0,0 +1,13 @@
+package ibf.web3dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web3dashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web3dashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ibf/web3dashboard/Web3dashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf.web3dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web3dashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
