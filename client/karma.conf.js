e4b98e0d83a7cdafa745e0b25fe3634a9405170e
John <JohnTeoJF+git@gmail.com>
2022-03-05 22:58:02 +0800

    Add Angular Template

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/web3-dashboard'),
+      dir: require('path').join(__dirname, './coverage/client'),
       subdir: '.',
       reporters: [
         { type: 'html' },
